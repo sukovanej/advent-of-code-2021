be24a58cae9255b548ee11678a97a76b695091f0
milan.suk <milan.suk@kiwi.com>
2021-12-05 11:27:38 +0100

    day 5

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,41 @@ pub mod day_1;
 pub mod day_2;
 pub mod day_3;
 pub mod day_4;
+pub mod day_5;
 
-use crate::day_4::{parse_input, solution2};
+use crate::day_5::{parse_input, solution};
+
+// use clap::Parser;
+use lazy_static::lazy_static;
+use std::collections::HashMap;
+
+lazy_static! {
+    static ref DAYS_WITH_INPUT_FILES: HashMap<u32, Vec<&'static str>> = {
+        let mut map = HashMap::new();
+        map.insert(1, vec!["input_1_1.txt", "input_1_test.txt"]);
+        map.insert(2, vec!["input_2_1.txt"]);
+        map.insert(3, vec!["input_3_1.txt", "input_3_test.txt"]);
+        map.insert(4, vec!["input_4_1.txt", "input_4_test.txt"]);
+        map
+    };
+}
+
+// #[derive(Parser)]
+// #[clap(version = "1.0", author = "Milan Suk <milansuk@email.cz>")]
+// struct Opts {
+//     #[clap(short, long, parse(from_occurrences))]
+//     day: u32,
+// }
 
 fn main() {
-    let file_name = "inputs/input_4_1.txt";
-    let (numbers, boards) = parse_input(file_name);
+    //let opts: Opts = Opts::parse();
 
-    println!("{}", solution2(&numbers, &boards));
+    let file_name = "inputs/input_5_1.txt";
+    let input = parse_input(file_name);
+
+    println!("{}", solution(&input, false));
 }
+
+// fn run_day_1(filename: &str) {
+//
+// }
