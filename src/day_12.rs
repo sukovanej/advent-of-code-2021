b2246e5aa66e7658121c11c6c544195c2851a847
milan.suk <milan.suk@kiwi.com>
2021-12-17 03:20:52 +0100

    day 12

--- src/day_12.rs
+++ src/day_12.rs
@@ -1,13 +1,113 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
-pub type Connections = HashMap<String, String>;
+pub type Connections = HashMap<String, HashSet<String>>;
 
-pub fn solution1(input: &Connections) -> u32 {
-    todo!()
+pub fn solution1(map: &Connections) -> usize {
+    let paths = get_paths(
+        vec!["start".to_string()],
+        map,
+        HashSet::from(["start".to_string()]),
+    );
+    paths.len()
+}
+
+pub fn solution2(map: &Connections) -> usize {
+    let paths = get_paths_2(vec!["start".to_string()], map, HashMap::new());
+
+    paths.len()
+}
+
+fn get_paths(
+    current_path: Vec<String>,
+    map: &Connections,
+    visited_small: HashSet<String>,
+) -> HashSet<Vec<String>> {
+    let current_node = current_path.last().unwrap().to_owned();
+    let next_possible_nodes = map[&current_node]
+        .iter()
+        .filter(|&i| !visited_small.contains(i))
+        .collect::<HashSet<&String>>();
+
+    let mut paths = HashSet::new();
+
+    for next in next_possible_nodes {
+        let mut new_path = current_path.clone();
+        new_path.push(next.clone());
+
+        if next == "end" {
+            paths.insert(new_path);
+            continue;
+        }
+
+        let mut new_visited_small = visited_small.clone();
+
+        if is_small(next) {
+            new_visited_small.insert(next.to_string());
+        }
+
+        let new_paths = get_paths(new_path, map, new_visited_small);
+        paths.extend(new_paths);
+    }
+
+    paths
+}
+
+fn get_paths_2(
+    current_path: Vec<String>,
+    map: &Connections,
+    visited_small: HashMap<String, u8>,
+) -> HashSet<Vec<String>> {
+    let current_node = current_path.last().unwrap().to_owned();
+    let single_cave_visited_used_limit = visited_small.values().any(|&i| i == 2);
+    let next_possible_nodes = map[&current_node]
+        .iter()
+        .filter(|&i| {
+            (!visited_small.contains_key(i) || !single_cave_visited_used_limit) && i != "start"
+        })
+        .collect::<HashSet<&String>>();
+
+    let mut paths = HashSet::new();
+
+    for next in next_possible_nodes {
+        let mut new_path = current_path.clone();
+        new_path.push(next.clone());
+
+        if next == "end" {
+            paths.insert(new_path);
+            continue;
+        }
+
+        let mut new_visited_small = visited_small.clone();
+
+        if is_small(next) {
+            *new_visited_small.entry(next.clone()).or_insert(0) += 1;
+        }
+
+        let new_paths = get_paths_2(new_path, map, new_visited_small);
+        paths.extend(new_paths);
+    }
+
+    paths
+}
+
+fn is_small(value: &str) -> bool {
+    value.chars().all(|i| i.is_lowercase())
 }
 
 pub fn parse_input(input: &str) -> Connections {
-    todo!()
+    input
+        .lines()
+        .map(|line| {
+            let split = line.split('-').collect::<Vec<&str>>();
+            (split[0].to_owned(), split[1].to_owned())
+        })
+        .fold(HashMap::new(), |mut acc, (from, to)| {
+            acc.entry(from.to_owned())
+                .or_insert_with(HashSet::new)
+                .insert(to.to_owned());
+            acc.entry(to).or_insert_with(HashSet::new).insert(from);
+            acc
+        })
 }
 
 #[cfg(test)]
@@ -17,7 +117,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_solution1_1() {
+    fn test_solution_1() {
         let content = "start-A
 start-b
 A-c
@@ -27,10 +127,11 @@ A-end
 b-end";
         let input = parse_input(&content);
         assert_eq!(solution1(&input), 10);
+        assert_eq!(solution2(&input), 36);
     }
 
     #[test]
-    fn test_solution1_2() {
+    fn test_solution_2() {
         let content = "dc-end
 HN-start
 start-kj
@@ -43,10 +144,11 @@ kj-HN
 kj-dc";
         let input = parse_input(&content);
         assert_eq!(solution1(&input), 19);
+        assert_eq!(solution2(&input), 103);
     }
 
     #[test]
-    fn test_solution1_3() {
+    fn test_solution_3() {
         let content = "fs-end
 he-DX
 fs-he
@@ -66,14 +168,17 @@ zg-he
 pj-fs
 start-RW";
         let input = parse_input(&content);
-        assert_eq!(solution1(&input), 19);
+        assert_eq!(solution1(&input), 226);
+        assert_eq!(solution2(&input), 3509);
     }
 
     #[test]
-    fn test_solution1_real_data() {
+    fn test_solution_real_data() {
         let content = read_to_string("inputs/input_12_1.txt").unwrap();
         let input = parse_input(&content);
-        assert_eq!(solution1(&input), 19);
-    }
+        assert_eq!(solution1(&input), 3000);
 
+        // TODO: is unefficient
+        // assert_eq!(solution2(&input), 74222);
+    }
 }
